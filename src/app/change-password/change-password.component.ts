f6c3c08e704ab6574945cc69f913eff71dc175db
GiovaniFZ <giovanifinazzi@gmail.com>
2024-06-20 11:04:29 -0300

    Verificar se a senha digitada é a mesma da confirmada

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -15,6 +15,7 @@ export class ChangePasswordComponent {
   loading: boolean = false;
   loading2: boolean = false;
   errorAt: boolean = false;
+  passNotConf:boolean = false;
   bearer = String(this.route.snapshot.paramMap.get('bearer'));
   phaseName = this.route.snapshot.paramMap.get('phaseName');
   errorMessage: string = '';
@@ -30,22 +31,34 @@ export class ChangePasswordComponent {
   newPass = '';
   newPassConf = '';
 
-  changePass(){
+  verify() {
+    // Verificar se a senha digitada é a mesma da confirmada
+    if (this.newPass === this.newPassConf) {
+      this.passNotConf = false;
+      this.changePass();
+    }else{
+      this.passNotConf = true;
+    }
+  }
+
+  changePass() {
     let json = {
       "newPassword": this.newPass
     }
+    this.passUpdated = false;
+    this.errorAt = false;
     this.loading = true;
     this.userDataService.handlePost(this.bearer, '/advisor/change-password', json).subscribe(
-      (response) =>{
+      (response) => {
         this.passUpdated = true;
         this.loading = false;
         console.log(response);
       },
-      (error)=>{
-        if(error.status === 200){
+      (error) => {
+        if (error.status === 200) {
           this.passUpdated = true;
           this.loading = false;
-        }else{
+        } else {
           this.errorAt = true;
           this.loading = false;
           console.log(error);
@@ -53,23 +66,23 @@ export class ChangePasswordComponent {
       }
     )
   }
-  
-  goBack(){
+
+  goBack() {
     this.loading2 = true;
     let teams = [];
     this.userDataService.handleAdvisor(this.bearer)
-        .subscribe(
-          (response) => {    
-            teams = response["teams"];
-            this.userDataService.setTeams(teams);
-            this.loading2 = false;
-            this.router.navigate(['/advisor', this.phaseName, this.bearer]);
-          },
-          (error) => {                              
-            console.error('Request failed with error')
-            this.errorMessage = error;
-            this.loading2 = false;
-            this.errorAt = true;
-          });
+      .subscribe(
+        (response) => {
+          teams = response["teams"];
+          this.userDataService.setTeams(teams);
+          this.loading2 = false;
+          this.router.navigate(['/advisor', this.phaseName, this.bearer]);
+        },
+        (error) => {
+          console.error('Request failed with error')
+          this.errorMessage = error;
+          this.loading2 = false;
+          this.errorAt = true;
+        });
   }
 }
